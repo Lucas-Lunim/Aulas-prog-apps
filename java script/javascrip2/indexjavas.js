93ce115aef3825eaeff0ceb05e3663bb78214f9e
Lucas-Lunim <lunimbr@gmail.com>
2026-02-11 10:50:56 -0300

    update

--- java script/javascrip2/indexjavas.js
+++ java script/javascrip2/indexjavas.js
@@ -1,21 +1,4 @@
 let square = document.getElementById('a1')
-
-function alternarcolor(){
-    square.style.background = "blue"
-    if (square.style.background == "blue")
-    {
-        square.style.background == "red"
-    }
-    else{
-        square.style.background == "blue"
-    }
-}
-
-square.addEventListener("click", alternarcolor)
-
-
-
-
 let inpu1 = document.getElementById("l1")
 
 inpu1.addEventListener("input", trocarcor)
