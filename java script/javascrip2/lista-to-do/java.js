8c8656ffe321d6ccc17050b3d6140519a2c5c546
Lucas-Lunim <lunimbr@gmail.com>
2026-02-12 11:16:08 -0300

    correção da to do list

--- java script/javascrip2/lista-to-do/java.js
+++ java script/javascrip2/lista-to-do/java.js
@@ -1,22 +1,59 @@
-let inputtarefa = document.getElementById("inputtarefa")
-let addbotao = document.getElementById("addbotao")
-let lista = document.getElementById("Lista")
+// let inputtarefa = document.getElementById("inputtarefa")
+// let addbotao = document.getElementById("addbotao")
+// let lista = document.getElementById("Lista")
 
-addbotao.addEventListener("click", add_tarefa)
+// addbotao.addEventListener("click", add_tarefa)
 
-function add_tarefa(){
-    let novo_item = inputtarefa.value.trim();
+// function add_tarefa(){
+//     let novo_item = inputtarefa.value.trim();
 
-    if (novo_item !== "") {
+//     if (novo_item !== "") {
         
-        let novoitem = document.createElement("li")
+//         let novoitem = document.createElement("li")
 
-        novoitem.innerHTML = novo_item
+//         novoitem.innerHTML = novo_item
 
-        lista.appendChild(novoitem)
+//         lista.appendChild(novoitem)
     
-        inputtarefa.value = "";
+//         inputtarefa.value = "";
 
-    }
+//     }
 
+// }
+
+// botaoexcluir = document.querySelector("Excluir")
+
+// botaoexcluir.addEventListener("click", function(){
+//     novo_item = this.parentElement;
+//     novo_item.remove();
+
+// })
+
+// Correção
+
+let btn_enviar = document.getElementById("enviar")
+
+function cadastrar(){
+    console.log("AISISISISI")
+
+    let lista = document.getElementById("lista")
+    let div = document.createElement("div")
+    let button = document.createElement("button")
+    let span = document.createElement("span")
+    let input = document.getElementById("input").value
+    
+    div.classList.add("item")
+    lista.appendChild(div)
+
+    span.innerHTML = input
+    button.textContent = "Deletar"
+
+    div.appendChild(span)
+    div.appendChild(button)
+
+    button.addEventListener("click", () => {
+        button.parentElement.remove()
+    })
 }
+
+btn_enviar.addEventListener("click", cadastrar)
