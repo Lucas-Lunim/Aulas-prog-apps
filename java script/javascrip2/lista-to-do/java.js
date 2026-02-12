1b18d38b628b36e7c5f126e8bcb391b6463d3393
Lucas-Lunim <lunimbr@gmail.com>
2026-02-12 09:07:14 -0300

    update to do list

--- java script/javascrip2/lista-to-do/java.js
+++ java script/javascrip2/lista-to-do/java.js
@@ -1,5 +1,22 @@
-let add = document.getElementById("tarefa")
+let inputtarefa = document.getElementById("inputtarefa")
+let addbotao = document.getElementById("addbotao")
+let itemlista = document.getElementById("itemlista")
+
+addbotao.addEventeListener("click", add_tarefa)
 
 function add_tarefa(){
+    novo_item = inputtarefa.value.trim();
+
+    if (novo_item !== "") {
+        
+        lista = document.createElement("lis");
     
+        lista.textcontent = novo_item;
+    
+        itemlista.appendChild("lista");
+    
+        inputtarefa.value = "";
+
+    }
+
 }
