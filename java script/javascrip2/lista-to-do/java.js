fcc3eec039f136edcfe69694c577b30db1d0d382
Lucas-Lunim <lunimbr@gmail.com>
2026-02-12 09:42:52 -0300

    todo list funcionando

--- java script/javascrip2/lista-to-do/java.js
+++ java script/javascrip2/lista-to-do/java.js
@@ -1,14 +1,13 @@
 let inputtarefa = document.getElementById("inputtarefa")
 let addbotao = document.getElementById("addbotao")
-let itemlista = document.getElementById("itemlista")
 let lista = document.getElementById("Lista")
 
-addbotao.addEventeListener("click", add_tarefa)
+addbotao.addEventListener("click", add_tarefa)
 
 function add_tarefa(){
     let novo_item = inputtarefa.value.trim();
 
-    // if (novo_item !== "") {
+    if (novo_item !== "") {
         
         let novoitem = document.createElement("li")
 
@@ -18,7 +17,6 @@ function add_tarefa(){
     
         inputtarefa.value = "";
 
-
-    // }
+    }
 
 }
